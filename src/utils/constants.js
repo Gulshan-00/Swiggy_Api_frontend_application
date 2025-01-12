1bf13abcdc9fb3dee9987207d78ab7aa4803aeb0
Gulshan-00 <rr3870044@gmail.com>
2025-01-12 19:33:26 +0530

    added cors proxy in the constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL="https://proxy.cors.sh/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 
 export const LOGO_URL="https://img.freepik.com/premium-vector/online-food-order-logo-icon_61778-45.jpg";
 
-export const MIND_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
+export const MIND_URL="https://proxy.cors.sh/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
 // export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId=";
 
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4684272&lng=78.53541179999999&restaurantId=";
+export const MENU_API="https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4684272&lng=78.53541179999999&restaurantId=";
